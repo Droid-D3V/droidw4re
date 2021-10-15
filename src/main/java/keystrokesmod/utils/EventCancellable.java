0b9092b8088bd39ddb8ca184969d9dc13cd58f8c
Droid-D3V <79214769+Droid-D3V@users.noreply.github.com>
2021-10-15 11:31:24 -0400

    Create EventCancellable.java

---
+++ src/main/java/keystrokesmod/utils/EventCancellable.java
@@ -0,0 +1,21 @@
+import keystrokesmod.utils.Cancellable;
+import keystrokesmod.utils.Event;
+
+public abstract class EventCancellable
+implements Event,
+Cancellable {
+    private boolean cancelled;
+
+    protected EventCancellable() {
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean state) {
+        this.cancelled = state;
+    }
+}
